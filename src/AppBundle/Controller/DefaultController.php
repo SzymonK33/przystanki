597fa0d2cf9ce6d379539e23e7154dec6d6b811d
szymonkirszniak <szymonkirszniak@gmail.com>
2021-04-29 12:14:08 +0200

    Dodawanie przystanka bez zalacznikow

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,8 +2,10 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Przystanki;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
@@ -18,4 +20,24 @@ class DefaultController extends Controller
             
         ]);
     }
+    /**
+     * @Route("/przystanki/add")
+     * Method({"POST"})
+     */
+    public function add(){
+        $entityManager = $this ->getDoctrine() ->getManager();
+        
+        $data= new Przystanki();
+        $data -> setAdresPrzystanku($_POST['adres']);
+        $data -> setOpis($_POST['opis']);
+        $data -> setIdentyfikator($_POST['identyfikator']);
+        
+        $entityManager->persist($data);
+        $entityManager->flush();
+        
+        return $this->render('default/addPrzystanek.html.twig', [
+            
+        ]);
+    }
+    
 }
