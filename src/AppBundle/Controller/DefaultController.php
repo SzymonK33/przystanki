f0dc030b02b712a2fad40763195abf25951dd3e0
szymonkirszniak <szymonkirszniak@gmail.com>
2021-04-30 16:31:18 +0200

    dodawanie zdjecia/ bez walidacji

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -34,15 +34,20 @@ class DefaultController extends Controller
             $data -> setAdresPrzystanku($_POST['adres']);
             $data -> setOpis($_POST['opis']);
             $data -> setIdentyfikator($_POST['identyfikator']);
-            $data ->setZal1($_FILES['zal1']['name']);
+            $data ->setOdczytano($_POST['odczytano']);
+            
+            //Operacje na zalaczniku 1
+            
+            /** @var UploadedFile $zal1*/
+            $zal1=$request ->files->get('zal1');
+            $destination = $this ->getParameter('kernel.project_dir').'/uploads';
+            $newName = uniqid().'-'.$zal1->getClientOriginalName();
+            $zal1->move($destination,$newName);
+            
+            $data ->setZal1($newName);
             var_dump($_FILES['zal1']);
             $entityManager->persist($data);
             $entityManager->flush();
-            
-            
-            $zal1=$request ->files->get('zal1');
-            $destination=$this->getParameter('kernel.project.dir').'/uploads';
-            $zal1->move($destination);
             
             return $this->render('default/addPrzystanek.html.twig', [
 
